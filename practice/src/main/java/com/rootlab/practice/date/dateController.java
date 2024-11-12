29364f6545f761a38bbef2489f05c02dc184c9fe
pinusdensiflora <dawon10050@daum.net>
2024-11-12 17:48:02 +0900

    1112

---
+++ practice/src/main/java/com/rootlab/practice/date/dateController.java
@@ -0,0 +1,17 @@
+package com.rootlab.practice.date;
+
+import org.checkerframework.common.reflection.qual.GetClass;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/date")
+public class dateController {
+
+	@GetMapping("")
+	public String date() {
+		return "date/date";
+	}
+	
+}
