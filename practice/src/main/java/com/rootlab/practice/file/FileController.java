2a2a4b27b60e79b6b760daea8ba872a43decbe7b
pinusdensiflora <dawon10050@daum.net>
2024-11-12 15:21:04 +0900

    2. 정규표현식 미적용

---
+++ practice/src/main/java/com/rootlab/practice/file/FileController.java
@@ -0,0 +1,16 @@
+package com.rootlab.practice.file;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/file")
+public class FileController {
+
+	@GetMapping("")
+	public String password() {
+		return "/file/file";
+	}
+
+}
