d6cd2107f222d6ea49e26f55a314e4028a355a15
pinusdensiflora <dawon10050@daum.net>
2024-11-19 17:10:15 +0900

    검색 버튼 숨기기 이외 작업  완료

--- practice/src/main/java/com/rootlab/practice/file/FileController.java
+++ practice/src/main/java/com/rootlab/practice/file/FileController.java
@@ -2,15 +2,25 @@ package com.rootlab.practice.file;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/file")
 public class FileController {
 
 	@GetMapping("")
-	public String password() {
+	public String file() {
 		return "/file/file";
+	}
+	@PostMapping("/upload")
+	public String file(@RequestParam("files1[]") MultipartFile[] files1,
+            			  @RequestParam("files2[]") MultipartFile[] files2) {
+		
+	
+		return "/file/file" ;
 	}
 
 }
