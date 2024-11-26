4a9f3761b00f358030e57dfe1c0c7833c0374ef4
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-11-26 14:18:00 +0900

    db 시행중

--- practice/src/main/java/com/rootlab/practice/db/DBController.java
+++ practice/src/main/java/com/rootlab/practice/db/DBController.java
@@ -1,16 +1,37 @@
 package com.rootlab.practice.db;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/db")
 public class DBController {
-
 	
+
 	@GetMapping("")
 	public String dbmain(){
 		return "/db/db";
 	}
+	
+//	@GetMapping("/api")
+//	public List<Person> api(){
+//		//return 
+//	}
+	
+	@PostMapping("/api")
+	public ResponseEntity<String> api(@RequestBody Map<String, String> person) {
+		System.out.println(person.toString());
+		
+		return ResponseEntity.ok("");
+	}
+	
+	
 }
