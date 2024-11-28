fe7215aadf3316341718cb7514ef1d43db0bdeac
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-11-28 10:51:53 +0900

    init; community practice project

---
+++ practcomm/src/main/java/com/rootlab/practcomm/PractcommApplication.java
@@ -0,0 +1,13 @@
+package com.rootlab.practcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractcommApplication.class, args);
+	}
+
+}
