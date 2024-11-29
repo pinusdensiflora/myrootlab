a7eaa1b56a3eadcfd9b09cc6f299b57ec498b0fd
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-11-29 18:02:22 +0900

    체크박스 클릭시 이벤트 미구현

--- practcomm/src/main/resources/static/js/video.js
+++ practcomm/src/main/resources/static/js/video.js
@@ -4,27 +4,32 @@ let page = 1;
 let is_end = false;
 
 
+let checked = [];
+
+
+
+
 
 function showLoading() {
-    document.getElementById("loading").style.display = "flex"; // 로딩 화면 보이기
+	document.getElementById("loading").style.display = "flex"; // 로딩 화면 보이기
 }
 
 function hideLoading() {
-    document.getElementById("loading").style.display = "none"; // 로딩 화면 안보이기
+	document.getElementById("loading").style.display = "none"; // 로딩 화면 안보이기
 }
 
 async function getData() {
-    showLoading(); // 데이터를 요청하기 전에 로딩 화면을 표시
-    try {
-        const response = await fetch(`http://localhost:8080/community/video/api?keyword=${keyword}&page=${page}`); // 데이터 요청
-        const data = await response.json();
-        console.log("data : ",data);
-        return data; // 데이터 반환
-    } catch (error) {
-        console.error("Error fetching data", error);
-    } finally {
-        hideLoading(); // 데이터 요청이 끝나면 로딩 화면을 숨기기
-    }
+	showLoading(); // 데이터를 요청하기 전에 로딩 화면을 표시
+	try {
+		const response = await fetch(`http://localhost:8080/community/video/api?keyword=${keyword}&page=${page}`); // 데이터 요청
+		const data = await response.json();
+		console.log("data : ", data);
+		return data; // 데이터 반환
+	} catch (error) {
+		console.error("Error fetching data", error);
+	} finally {
+		hideLoading(); // 데이터 요청이 끝나면 로딩 화면을 숨기기
+	}
 }
 
 
@@ -36,25 +41,41 @@ async function getresult() {
 		const meta = ajaxResult["meta"];
 		const result = ajaxResult["documents"];
 		end = meta.is_end;
-		
+
 		console.log(result);
 		console.log(meta);
 		rend(result);
-		
+
 	} catch (error) {
 		console.error("Error: ", error);  // 오류 처리
 	}
-		
+
 }
 
-function rend(result){
+function rend(result) {
 	const section = document.getElementById("section");
 	section.innerHTML = "";
-	result.forEach(function(item){
-		
-		section.innerHTML = section.innerHTML + `<div class="col-3"><input type="checkbox" value = "${item.url}"><img src="${item.thumbnail}"></div>`;
-		
-		
+	result.forEach(function(item, index) {
+		section.innerHTML = section.innerHTML
+			+ `<div class="col-3"><input type="checkbox" name="save" value = "${index}"><img src="${item.thumbnail}"></div>`;
+
 	});
-	
+
+
+/*	let checkboxs = document.getElementsByName("save");
+	//checkboxs.addEventListner("click", checkedBoxAll); //element가 모인 nodeList 라서 불가능 하나씩 걸어줘야함
+	checkboxs.forEach(function(checkbox) {
+		checkbox.addEventListener("click", checkedBoxAll);
+	});*/
+
 }
+
+function checkedBoxAll() {
+	const query = 'input[name="save"]:checked';
+	const selectedEls = document.querySelector(query);
+
+
+	console.log(selectedEls);
+}
+
+
