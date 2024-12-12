709b2e2adb4781b202a033d7725abba32b1d5b3c
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-12-12 17:53:04 +0900

    reserve 아주조금 시작

---
+++ practcomm/src/main/java/com/rootlab/practcomm/reserve/reserveController.java
@@ -0,0 +1,13 @@
+package com.rootlab.practcomm.reserve;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class reserveController {
+
+	@GetMapping("/reserve")
+	public String reserve() {
+		return "reserve";
+	}
+}
